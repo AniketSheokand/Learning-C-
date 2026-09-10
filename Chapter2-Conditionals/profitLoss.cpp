02c3023405979f2226637ede79a962b26b80de9d
AniketSheokand <aniketsheokand0@outlook.com>
2026-09-10 21:46:24 +0530

    Program for calculating profit loss for a retailer

---
+++ Chapter2-Conditionals/profitLoss.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float cp , sp;
+    cout<<"Enter the Cost Price of the product: ";
+    cin>>cp;
+    cout<<"Enter the Selling Price of the product: ";
+    cin>>sp;
+    if(sp>cp){
+        cout<<"Profit of Rupees "<<sp-cp<<endl;
+        cout<<"Profit of "<<(sp-cp)/cp*100<<" %";
+    }
+    else{
+        cout<<"Loss of Rupees "<<cp-sp<<endl;
+        cout<<"Loss of "<<(cp-sp)/cp*100<<" %";
+    }
+}
