e0e09ea9c91266b1b42b8d10e39aa9639cd6a39f
AniketSheokand <aniketsheokand0@outlook.com>
2026-09-10 14:15:53 +0530

    Added EvenOdd program

---
+++ Chapter2-Conditionals/evenOdd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter Your number: ";
+    cin>>x;
+    if (x%2==0){
+        cout<<"The provided number is Even";
+    }
+    else{
+        cout<<"The provided number is Odd";
+    } 
+}
