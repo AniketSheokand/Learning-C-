a125332cebf9b6d51c046d56463329ae7f586060
AniketSheokand <aniketsheokand0@outlook.com>
2026-09-11 22:49:37 +0530

    Added smallestNumber.cpp (conditionals practice)

---
+++ Chapter2-Conditionals/smallestNumber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b , c;
+    cout<<"Enter your 1st number: ";
+    cin>>a;
+    cout<<"Enter your 2nd number: ";
+    cin>>b;
+    cout<<"Enter your 3rd number: ";
+    cin>>c;
+    if(a<b && a<c){
+        cout<<"The smallest number is "<<a;
+    }
+    else if(b<a && b<c){
+        cout<<"The smallest number is "<<b;
+    }
+    else{
+        cout<<"The smallest number is "<<c;
+    }
+}
