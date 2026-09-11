95e827bb3920f87932b708e7d947c5dca68693b7
AniketSheokand <aniketsheokand0@outlook.com>
2026-09-11 23:26:16 +0530

    solved using nested if else

--- Chapter2-Conditionals/greatestNumber.cpp
+++ Chapter2-Conditionals/greatestNumber.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
-
+//-----------------1st Method Using normal if else------------------
+// int main(){
+//     int a , b , c;
+//     cout<<"Enter your 1st number: ";
+//     cin>>a;
+//     cout<<"Enter your 2nd number: ";
+//     cin>>b;
+//     cout<<"Enter your 3rd number: ";
+//     cin>>c;
+//     if(a>=b && a>=c){
+//         cout<<"The greatest number is "<<a;
+//     }
+//     else if(b>=a && b>=c){
+//         cout<<"The greatest number is "<<b;
+//     }
+//     else{
+//         cout<<"The greatest number is "<<c;
+//     }
+// }
+//----------------------2nd Method Using nested if else------------------------
 int main(){
     int a , b , c;
     cout<<"Enter your 1st number: ";
@@ -9,13 +28,20 @@ int main(){
     cin>>b;
     cout<<"Enter your 3rd number: ";
     cin>>c;
-    if(a>b && a>c){
-        cout<<"The greatest number is "<<a;
-    }
-    else if(b>a && b>c){
-        cout<<"The greatest number is "<<b;
+    if(a>=b){
+        if(a>=c){
+            cout<<"The greatest number is "<<a;
+        }
+        else{
+            cout<<"The greatest number is "<<c;
+        }
     }
     else{
-        cout<<"The greatest number is "<<c;
+        if(b>=c){
+            cout<<"The greatest number is "<<b;
+        }
+        else{
+            cout<<"The greatest number is "<<c;
+        }
     }
 }
