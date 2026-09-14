019c417f4a1e09040947f02804353f1a3df60508
AniketSheokand <aniketsheokand0@outlook.com>
2026-09-14 23:06:23 +0530

    Switch Function

---
+++ Chapter2-Conditionals/calculatiorFor2Inputs.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+    char op;
+    cin>>a;
+    cin>>op;
+    cin>>b;
+    switch(op){
+        case '+':
+            cout<<a+b;
+            break;
+        case '-':
+            cout<<a-b;
+            break;
+        case '*':
+            cout<<a*b;
+            break;
+        case '/':
+            cout<<a/b;
+            break;
+        default:
+            cout<<"Invalid Operator"; 
+    }
+}
